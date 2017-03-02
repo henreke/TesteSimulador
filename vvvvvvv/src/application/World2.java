79b57ca6337c435385a9e1d1964da3a366cbd626
kbqk <kbqk@MI00245747.petrobras.biz>
2017-03-02 15:38:24 -0300

    teste apenas do vapor5 e criado a classe World2 para futura implementação

---
+++ vvvvvvv/src/application/World2.java
@@ -0,0 +1,27 @@
+package application;
+
+import org.jbox2d.common.Vec2;
+
+public class World2 extends org.jbox2d.dynamics.World{
+
+	public World2(Vec2 gravity) {
+		super(gravity);
+		// TODO Auto-generated constructor stub
+
+	}
+
+	// TODO
+	/**
+	 *Aqui vamos criar uma lista dos Body que serão criados no World2
+	 *Os corpos vão possuir uma propriedada dos pixels
+	 *seria uma boa tambem o World2 possuir a Scena que ele vai brincar
+	 *Essa classe seria responsavel por atualizar a scena
+	 *pelos bodys
+	 *colocar uma funcao pra ir avancando o tempo
+	 *ter uma propriedade do tamanho da tela
+	 */
+
+
+
+
+}
