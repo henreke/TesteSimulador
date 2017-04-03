b54781fec32023a10a122b3b9a6e7ede98a5f628
henreke <henreke@hotmail.com>
2017-04-03 11:39:38 -0300

    tabela ok

--- vvvvvvv/src/application/GraficoController.java
+++ vvvvvvv/src/application/GraficoController.java
@@ -1,5 +1,9 @@
 package application;
 
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
@@ -11,6 +15,17 @@ public class GraficoController {
 
 	@FXML
 	private LineChart grafico;
+	
+	@FXML
+	private TableView<XYChart.Data<Float, Float>> tabela;
+	
+	@FXML
+	private TableColumn<XYChart.Data<Float, Float>,Float> tempoColuna;
+	
+	@FXML
+	private TableColumn<XYChart.Data<Float, Float>,Float> variavelColuna;
+	
+	private ObservableList<XYChart.Data<Float, Float>> listatabela = FXCollections.observableArrayList();
 
 	public GraficoController(){
 
@@ -25,8 +40,21 @@ public class GraficoController {
     	grafico.setCreateSymbols(false);
         grafico.getXAxis().setAutoRanging(true);
     	grafico.getYAxis().setAutoRanging(true);
-    	//Tesrte
     	grafico.getData().add(pontos_grafico);
+    	
+    	//Tabela
+    	listatabela = pontos_grafico.getData();
+    	tempoColuna.setCellValueFactory(cellData -> cellData.getValue().XValueProperty());
+    	variavelColuna.setCellValueFactory(cellData -> cellData.getValue().YValueProperty());
+    	tabela.setItems(listatabela);
+    	int i = 0;
+    	
     }
 
+}
+
+class Dados{
+	
+	public float tempo;
+	public float variavel;
 }
